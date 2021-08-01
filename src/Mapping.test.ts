4ec3073d5efd6f4679e8c897b95ff2ae5a72dea3
aquila.ziedins <aquila.ziedins@student.kuleuven.be>
2021-08-01 23:06:05 +0200

    new feature: give some keys to convert json objects

--- src/Mapping.test.ts
+++ src/Mapping.test.ts
@@ -1,128 +1,178 @@
 import { json2Obj, obj2Json } from './Mapping';
 
-test('Map conversion in json', () => {
-    interface TestObject {
-        value1: number,
-        value2: number
-    }
+interface TestObject {
+    value1: number,
+    value2: number
+}
 
-    interface TestObject2 {
-        txt1: string,
-        txt2: string,
-        list: number[],
-        list2: string[]
-    }
+interface TestObject2 {
+    txt1: string,
+    txt2: string,
+    list: number[],
+    list2: string[]
+}
 
-    interface SubMaps {
-        map1: Map<string, Map<string, string>>
-        map2: Map<string, TestObject>,
-        map3: Map<string, Set<string>>
-    }
+interface SubMaps {
+    map1: Map<string, Map<string, string>>
+    map2: Map<string, TestObject>,
+    map3: Map<string, Set<string>>
+}
 
-    interface SubMaps2 {
-        map1: Record<string, Record<string, string>>
-        map2: Record<number, TestObject>
-    }
+interface SubMaps2 {
+    map1: Record<string, Record<string, string>>
+    map2: Record<number, TestObject>
+}
 
-    interface IMap {
-        txt: string,
-        submaps: SubMaps
-        extraTxt: TestObject2,
-        set: Set<string>
-    }
+interface IMap {
+    txt: string,
+    submaps: SubMaps
+    extraTxt: TestObject2,
+    set: Set<string>
+}
 
-    const a = new Map([
-        ['a', 'aa'],
-        ['b', 'bb'],
-        ['c', 'cc']
-    ])
+const a = new Map([
+    ['a', 'aa'],
+    ['b', 'bb'],
+    ['c', 'cc']
+])
 
-    const b = new Map([
-        ['d', 'dd'],
-        ['e', 'ee'],
-        ['f', 'ff']
-    ])
+const b = new Map([
+    ['d', 'dd'],
+    ['e', 'ee'],
+    ['f', 'ff']
+])
 
-    const set = new Set(['hey', 'hallo', 'hoy']);
-    const set2 = new Set(['bye', 'goodbye', '👋']);
+const set = new Set(['hey', 'hallo', 'hoy']);
+const set2 = new Set(['bye', 'goodbye', '👋']);
 
-    const testObj: IMap = {
-        txt: 'txt1',
-        submaps: {
-            map1: new Map([
-                ['map1.1.', a],
-                ['map1.2.', a],
-                ['map1.3.', b]
-            ]),
-            map2: new Map([
-                ['1', {value1: 2, value2: 1}],
-                ['2', {value1: 2, value2: 2}],
-                ['3', {value1: 2, value2: 3}]
-            ]),
-            map3: new Map([
-                ['s1', set],
-                ['s2', set2]
-            ])
-        },
-        extraTxt: {
-            txt1: 'hello',
-            txt2: 'world',
-            list: [1,2,3,4,5,6],
-            list2: ['t', 'j'],
-        },
-        set: new Set(['a', 'b', 'c'])
-    }
+const testObj: IMap = {
+    txt: 'txt1',
+    submaps: {
+        map1: new Map([
+            ['map1.1.', a],
+            ['map1.2.', a],
+            ['map1.3.', b]
+        ]),
+        map2: new Map([
+            ['1', {value1: 2, value2: 1}],
+            ['2', {value1: 2, value2: 2}],
+            ['3', {value1: 2, value2: 3}]
+        ]),
+        map3: new Map([
+            ['s1', set],
+            ['s2', set2]
+        ])
+    },
+    extraTxt: {
+        txt1: 'hello',
+        txt2: 'world',
+        list: [1,2,3,4,5,6],
+        list2: ['t', 'j'],
+    },
+    set: new Set(['a', 'b', 'c'])
+}
 
-    const record = {
-        "txt":"txt1",
-        "submaps":{
-            "map1":{
-                "map1.1.":{
-                    "a":"aa",
-                    "b":"bb",
-                    "c":"cc",
-                    "_map_":true
-                },
-                "map1.2.":{
-                    "a":"aa",
-                    "b":"bb",
-                    "c":"cc",
-                    "_map_":true
-                },
-                "map1.3.":{
-                    "d":"dd",
-                    "e":"ee",
-                    "f":"ff",
-                    "_map_":true
-                },
+const record = {
+    "txt":"txt1",
+    "submaps":{
+        "map1":{
+            "map1.1.":{
+                "a":"aa",
+                "b":"bb",
+                "c":"cc",
                 "_map_":true
             },
-            "map2":{
-                "1":{"value1":2,"value2":1},
-                "2":{"value1":2,"value2":2},
-                "3":{"value1":2,"value2":3},
+            "map1.2.":{
+                "a":"aa",
+                "b":"bb",
+                "c":"cc",
                 "_map_":true
             },
-            "map3":{
-                "s1":{"_set_":["hey","hallo","hoy"]},
-                "s2":{"_set_":["bye","goodbye","👋"]},
+            "map1.3.":{
+                "d":"dd",
+                "e":"ee",
+                "f":"ff",
                 "_map_":true
             },
+            "_map_":true
         },
-        "extraTxt":{
-            "txt1":"hello",
-            "txt2":"world",
-            "list":[1,2,3,4,5,6],
-            "list2":["t","j"]
+        "map2":{
+            "1":{"value1":2,"value2":1},
+            "2":{"value1":2,"value2":2},
+            "3":{"value1":2,"value2":3},
+            "_map_":true
         },
-        "set":{
-            "_set_":['a','b','c']
-        }
+        "map3":{
+            "s1":{"_set_":["hey","hallo","hoy"]},
+            "s2":{"_set_":["bye","goodbye","👋"]},
+            "_map_":true
+        },
+    },
+    "extraTxt":{
+        "txt1":"hello",
+        "txt2":"world",
+        "list":[1,2,3,4,5,6],
+        "list2":["t","j"]
+    },
+    "set":{
+        "_set_":['a','b','c']
     }
+}
 
+const record2 = {
+    "txt":"txt1",
+    "submaps":{
+        "map1":{
+            "map1.1.":{
+                "a":"aa",
+                "b":"bb",
+                "c":"cc"
+            },
+            "map1.2.":{
+                "a":"aa",
+                "b":"bb",
+                "c":"cc"
+            },
+            "map1.3.":{
+                "d":"dd",
+                "e":"ee",
+                "f":"ff"
+            }
+        },
+        "map2":{
+            "1":{"value1":2,"value2":1},
+            "2":{"value1":2,"value2":2},
+            "3":{"value1":2,"value2":3}
+        },
+        "map3":{
+            "s1":["hey","hallo","hoy"],
+            "s2":["bye","goodbye","👋"]
+        },
+    },
+    "extraTxt":{
+        "txt1":"hello",
+        "txt2":"world",
+        "list":[1,2,3,4,5,6],
+        "list2":["t","j"]
+    },
+    "set":['a','b','c']
+}
+
+test('Convert json to and from a complex datastructure by using identifiers.', () => {
     const json = obj2Json(testObj);
-    expect(json).toStrictEqual(JSON.stringify(record)); //record should be converted to json to compare
+    expect(json).toStrictEqual(JSON.stringify(record));
 
     const original = json2Obj(json) as IMap;
     expect(original).toStrictEqual(testObj);
 });
+
+test('Convert json to and from a complex datastructure by using the given keys as identifiers.', () => {
+    const json = obj2Json(testObj, true);
+    expect(json).toStrictEqual(JSON.stringify(record2));
+
+    const original = json2Obj(json, {
+        Map: ['map1', 'map2', 'map3', 'map1.1.', 'map1.2.', 'map1.3.'], 
+        Set: ['set', 's1', 's2']
+    });
+    expect(original).toStrictEqual(testObj);
+})
--- src/Mapping.ts
+++ src/Mapping.ts
@@ -3,74 +3,107 @@ const identifiers = {
     Set: '_set_'
 }
 
+interface Keys {
+    Map?: string[],
+    Set?: string[]
+}
+
 /**
  * Convert an object with nested maps and sets to json. This function will add a _map_ identifier to the records.
  * When converting the json back to an object with nesting, that identifier is neccessary to reconstruct the original object.
+ * The difference between obj2Json with noIdentifier = true and json.stringify is that the first will convert a map to an object
+ * with all its props and json.stringify will do the conversion to an empty object.
  * @param obj object with nested maps/sets
+ * @param noIdentifier Determines whether or not the json should have identifiers.
  * @returns json
  */
-export function obj2Json(obj: object): string{
-    return JSON.stringify(obj, (_, value) => (typeof value === 'object' && value !== null) ? replacer(value) : value);
+export function obj2Json(obj: object, noIdentifier?: boolean): string{
+    return JSON.stringify(obj, (_, value) => (typeof value === 'object' && value !== null) ? replacer(value, noIdentifier) : value);
 }
 
 /**
- * Convert a json to an object with nested maps/sets. Conversion can only be achieved if the objects in the json
- * have the _map_/_set_ identifier.
+ * Convert a json to an object with nested maps/sets. Conversion can be achieved in two ways.
+ * 1) If the objects in the json have the _map_/_set_ identifier.
+ * 2) When keysToConvert is specified. 
+ * If the json structure has multiple keys with the same name and that name was specified in the keysToConvert than all those keys 
+ * will be converted accordingly. If an object in the json has an identifier and the key of that object was specified in keysToConvert
+ * than conversion with identifier will come first because the identifiers shouldn't be visible in the resulting object.
  * @param json json that will be converted to an object
  * @returns an object
  */
-export function json2Obj(json: string) {
-    return JSON.parse(json, reviver);
+export function json2Obj(json: string, keysToConvert?: Keys) {
+    return JSON.parse(json, (key: any, value: any) => reviver(key, value, keysToConvert));
 }
 
 //Replace sets and maps to an object with identifier.
-function replacer(value: object): Record<string, any> | object{
+function replacer(value: object, noIdentifier?: boolean): Record<string, any> | object{
     if(value instanceof Map){
         let record: Record<string, any> = Array.from(value.entries()).reduce((main, [key, value]) => ({...main, [key]: value}), {});
-        record[identifiers.Map] = true;
+        if(noIdentifier !== true) record[identifiers.Map] = true;
         return record;
-    } else if(value instanceof Set) {
-        return {_set_: Array.from(value.values())}
-    } else {
-        const record: Record<string, any> = value;
-        return record;
-    }    
+    } 
+    
+    if(value instanceof Set) {
+        return noIdentifier === true ? 
+            Array.from(value.values()) : {_set_: Array.from(value.values())};
+    }
+    
+    const record: Record<string, any> = value;
+    return record;
 }
 
 //Replace objects with identifier to an instance of map or set.
-function reviver(_: any, value: any) {
+function reviver(key: any, value: any, keysToConvert?: Keys) {
     if(typeof value === 'object' && value !== null){
         if(identifiers.Map in value){
-            const map = new Map();
-
-            Object.entries(value).forEach((item) => {
-                if(item[0] === identifiers.Map){
-                    if(typeof item[1] === 'boolean'){
-                        if(!item[1]) map.set(item[0], item[1]);
-                    } else{
-                        throw new Error('The value of key: _map_ should be a boolean.')
-                    }
-                } else{
-                    map.set(item[0], item[1]);
-                }
-            })
-
-            return map;
+            return convertObj2Map(value);
         }
 
         if(identifiers.Set in value){
-            const set = new Set();
             const obj = Object.values(value);
+            return convertObj2Set(obj[0]);
+        }
 
-            if(obj[0] instanceof Array){
-                Array.from(obj[0]).forEach(item => set.add(item));
-            } else {
-                throw new Error('The value of key: _set_ should be an array.');
+        if(keysToConvert){
+            if(keysToConvert.Map && keysToConvert.Map.find(i => i === key)){
+                return convertObj2Map(value);
             }
 
-            return set;
+            if(keysToConvert.Set && keysToConvert.Set.find(i => i === key)){
+                return convertObj2Set(value);
+            }
         }
     }
 
     return value;
+}
+
+function convertObj2Map(value: object){
+    const map = new Map();
+
+    Object.entries(value).forEach((item) => {
+        if(item[0] === identifiers.Map){
+            if(typeof item[1] === 'boolean'){
+                if(!item[1]) map.set(item[0], item[1]);
+            } else{
+                throw new Error('The value of key: _map_ should be a boolean.')
+            }
+        } else{
+            map.set(item[0], item[1]);
+        }
+    })
+
+    return map;
+}
+
+function convertObj2Set(obj: unknown){
+    const set = new Set();
+
+    if(obj instanceof Array){
+        Array.from(obj).forEach(item => set.add(item));
+    } else {
+        throw new Error('The value of key: _set_ should be an array.');
+    }
+
+    return set;
 }
